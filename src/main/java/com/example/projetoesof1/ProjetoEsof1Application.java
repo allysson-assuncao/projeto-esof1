3f420c235c28ddcfaeab5124df3bddc4ab4d5774
allysson-assuncao <anybody723@proton.me>
2025-04-24 09:53:08 -0300

    Initial commit 2

---
+++ src/main/java/com/example/projetoesof1/ProjetoEsof1Application.java
@@ -0,0 +1,13 @@
+package com.example.projetoesof1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEsof1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoEsof1Application.class, args);
+    }
+
+}
